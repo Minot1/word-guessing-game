bec38b099e40adf8ab873ac8f426096dde65a015
Minot1 <dincer.taner123@gmail.com>
2023-06-05 20:52:08 +0300

    add sort to guess

--- ui/src/GuessPage.js
+++ ui/src/GuessPage.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CustomBar from './CustomBar'
 import { Grid, TextField } from "@mui/material";
 
+const data = [
+    {percent: "75", word: "deneme1"},
+    {percent: "30", word: "deneme2"},
+    {percent: "5", word: "deneme3"},
+    {percent: "50", word: "deneme4"},
+    {percent: "100", word: "deneme5"},
+    {percent: "24", word: "deneme6"},
+]
+
 function GuessPage() {
+    const [mockData, setMockData] = useState([]);
+
+useEffect(() => {
+    console.log([...data].sort((a, b) => (parseInt(a.percent) < parseInt(b.percent)) ? 1 : -1));
+}, [])
+
+
   return (
     <div style={{ backgroundColor: "#303030", height: "100vh", padding: 20 }}>
         <Grid container justifyContent="center" alignItems="center" direction="column" p={2}>
